6cff78a8cbd4f587b5b246326284e9c861c8facb
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-12-05 22:04:44 -0300

    Initial commit

---
+++ src/test/java/br/com/lima/TabelaFipe/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lima.TabelaFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
