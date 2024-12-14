f237d552e8f10e32a06a5dbc5c57acae6dee721d
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-12-13 22:46:16 -0300

    Buscando todas as avaliações por veículo05 Commit

--- src/main/java/br/com/lima/TabelaFipe/model/Veiculo.java
+++ src/main/java/br/com/lima/TabelaFipe/model/Veiculo.java
@@ -1,4 +1,14 @@
 package br.com.lima.TabelaFipe.model;
 
-public record Veiculo() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Veiculo(
+        @JsonAlias("Valor") String valor,
+        @JsonAlias("Marca") String marca,
+        @JsonAlias("Modelo") String modelo,
+        @JsonAlias("anoModelo") Integer ano,
+        @JsonAlias("Combustivel") String tipoCombustivel
+) {
 }
